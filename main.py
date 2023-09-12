97167b8ef1148bcd79379074895f7c49e21a9c17
Hoyoung Jung <honux77@gmail.com>
2023-09-12 14:17:29 +0900

    그리드 레이아웃으로 레이아웃 업데이트

--- main.py
+++ main.py
@@ -12,30 +12,13 @@ win = tk.Tk()
 # Add a title
 win.title("Famliy Pocket Rom Manager")
 
-# Set size of window
-win.geometry("1024x768")
-
-# Add close button
-def _quit():
-    win.quit()
-    win.destroy()
-    exit()
-
-
-
-# 롬 목록 드롭리스트 추가
-label = ttk.Label(win, text="롬 목록 선택")
-label.pack()
-
 # roms 폴더의 서브 폴더들을 읽어서 드롭리스트에 추가
 import os
 from os import path
 subdirs = [f for f in os.listdir('roms/') if path.isdir(path.join('roms/', f))]
 
-romBox = ttk.Combobox(win, values=subdirs)
-#set default value to first item
-romBox.current(0)
-romBox.pack()
+# Set size of window
+win.geometry("1024x768")
 
 # add Button for selected romBox
 def listSelectedDir():
@@ -49,47 +32,58 @@ def listSelectedDir():
     # add romList to romListBox
     romListBox.delete(0, tk.END)    
 
+    #delete noImageTextBox
+    noImageTextBox.delete(1.0, tk.END)
+
     for rom in romList:
         romName = path.splitext(rom)[0]
-        romListBox.insert(tk.END, rom)            
-        noImgList = ""
+        romListBox.insert(tk.END, rom)                    
         
         if romName not in imgNameList:
             romListBox.itemconfig(tk.END, {'bg':'red'})
-            noImgList += rom + "\n"
+            noImageTextBox.insert(tk.INSERT, rom + "\n")
 
         
     # resize romListBox
-    romListBox.config(height=len(romList), width= 50)
+    romListBox.config(height=30, width= 50)
     # set default value to first item
     romListBox.select_set(0)
     # show romListBox
-    romListBox.pack()
-
-    # add no image list to scrolledtext
-    noImageTextBox.delete(1.0, tk.END)
-    noImageTextBox.insert(tk.INSERT, noImgList)
-    noImageTextBox.pack()
-    
 
 
+
+# 라벨들
+label = ttk.Label(win, text="롬 폴더")
+label.grid(column=0, row=0, pady=5, padx=5)
+
+label2 = ttk.Label(win, text="불러온 롬 리스트")
+label2.grid(column=0, row=2, pady=5, padx=5)
+
+label3 = ttk.Label(win, text="이미지가 없는 롬들")
+label3.grid(column=2, row=2, pady=5, padx=5)
+
+
+# 폴더 선택 콤보 박스
+romBox = ttk.Combobox(win, values=subdirs)
+#set default value to first item
+romBox.current(0)
+romBox.grid(column=0, row=1, padx=5, pady=5)
     
 
 # 선택 버튼을 추가하고 이 버튼을 클릭하면 선택된 항목을 표시하는 함수를 호출합니다.
 select_button = ttk.Button(win, text="선택", command=listSelectedDir)
-select_button.pack()
+select_button.grid(column=1, row=1)
 
-# 선택 결과를 표시할 listBox 추가
+# 롬 리스트용 리스트 박스
 from tkinter import Listbox
 romListBox = Listbox(win)
-# hide romListBox
-romListBox.pack_forget()
+romListBox.grid(column=0, row=3)
 
 
 # 이미지가 없는 롬 목록 출력용
 from tkinter import Text
 noImageTextBox = Text(win)
-noImageTextBox.pack_forget()
+noImageTextBox.grid(column=2, row=3)
 
 # 애플리케이션을 실행합니다.
 win.mainloop()
