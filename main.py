a4e76597df74dde5e936ca7074cb2ac1e3cc57ad
Hoyoung Jung <honux77@gmail.com>
2023-09-12 18:55:58 +0900

    어쩌다 보니 1차 기능 완성

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox as mBox
 
+from PIL import ImageTk, Image
+
 # Create instance
 win = tk.Tk()
 
@@ -20,8 +22,8 @@ subdirs = [f for f in os.listdir('roms/') if path.isdir(path.join('roms/', f))]
 # Set size of window
 win.geometry("1024x768")
 
-# add Button for selected romBox
-def listSelectedDir():
+# event handler for select_button
+def listSelectedDir(event):
     dir = romBox.get()
     import fileUtil
     romList = fileUtil.getRomList(dir)
@@ -42,14 +44,28 @@ def listSelectedDir():
         if romName not in imgNameList:
             romListBox.itemconfig(tk.END, {'bg':'red'})
             noImageTextBox.insert(tk.INSERT, rom + "\n")
-
-        
-    # resize romListBox
-    romListBox.config(height=30, width= 50)
+                        
     # set default value to first item
     romListBox.select_set(0)
+    romListBox.event_generate("<<ListboxSelect>>")
     # show romListBox
 
+# simple function for printing debug message
+def debug(msg):
+    debugLabel.configure(text=msg)
+
+# event handler for romListBox
+def romListBoxSelectHandler(event):
+    romFile = romListBox.get(romListBox.curselection())
+    import imgUtil
+    imageTk = imgUtil.findImageFromRomName(romBox.get(), romFile)
+    if (imageTk != None):        
+        imgLabel.configure(image=imageTk)
+        imgLabel.image = imageTk
+        imgInfoLabel.configure(text="{}: {} X {}".format(romFile, imageTk.width(), imageTk.height()))
+    else:
+        imgLabel.configure(image=baseImageTk)
+        imgInfoLabel.configure(text="Image Not Found") 
 
 
 # 라벨들
@@ -62,28 +78,44 @@ label2.grid(column=0, row=2, pady=5, padx=5)
 label3 = ttk.Label(win, text="이미지가 없는 롬들")
 label3.grid(column=2, row=2, pady=5, padx=5)
 
+label4 = ttk.Label(win, text="이미지 미리 보기")
+label4.grid(column=0, row=4, pady=5, padx=5)
+
+#load base Image
+baseImageTk = ImageTk.PhotoImage(Image.open("images/base.png"))
+
+imgLabel = ttk.Label(win, image=baseImageTk)
+imgLabel.grid(column=0, row=5, pady=5, padx=5)
+
+imgInfoLabel = ttk.Label(win, text="Image Info")
+imgInfoLabel.grid(column=2, row=5, pady=5, padx=5)
+
+# debug label
+debugLabel = ttk.Label(win, text="debug")
+debugLabel.grid(column=0, row=6, pady=5, padx=5)
 
 # 폴더 선택 콤보 박스
 romBox = ttk.Combobox(win, values=subdirs)
 #set default value to first item
 romBox.current(0)
 romBox.grid(column=0, row=1, padx=5, pady=5)
-    
-
-# 선택 버튼을 추가하고 이 버튼을 클릭하면 선택된 항목을 표시하는 함수를 호출합니다.
-select_button = ttk.Button(win, text="선택", command=listSelectedDir)
-select_button.grid(column=1, row=1)
+romBox.bind("<<ComboboxSelected>>", listSelectedDir)
 
 # 롬 리스트용 리스트 박스
 from tkinter import Listbox
 romListBox = Listbox(win)
-romListBox.grid(column=0, row=3)
+romListBox.config(height=20, width= 50)
+romListBox.bind('<<ListboxSelect>>', romListBoxSelectHandler)
+romListBox.grid(column=0, row=3, padx=5, pady=5)
 
 
 # 이미지가 없는 롬 목록 출력용
 from tkinter import Text
 noImageTextBox = Text(win)
-noImageTextBox.grid(column=2, row=3)
+noImageTextBox.config(height=20, width= 50)
+noImageTextBox.grid(column=2, row=3, padx=5, pady=5)
+
+romBox.event_generate("<<ComboboxSelected>>")
 
 # 애플리케이션을 실행합니다.
 win.mainloop()
